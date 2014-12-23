f333ea65e1f71099ce371f1f9cc36d3d18556e3e
Peter Mrekaj <mrekucci@gmail.com>
2014-12-23 21:41:40 +0100

    Solution of problem 2
    https://projecteuler.net/problem=2

---
+++ problem002/problem002.go
@@ -0,0 +1,24 @@
+// Copyright 2014 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package problem002
+
+// Solution for finding the sum of the even-valued terms in the
+// Fibonacci sequence whose values do not exceed four million.
+func Solution() int {
+	sum := 0
+	// Every 3rd number is even, so:
+	// 1, 1, 2, 3, 5, 8, ...
+	// a, b, c, a, b, c, ...     where c = a + b
+	a := 1
+	b := 1
+	c := a + b
+	for c <= 4e6 {
+		sum += c
+		a = b + c
+		b = c + a
+		c = a + b
+	}
+	return sum
+}
