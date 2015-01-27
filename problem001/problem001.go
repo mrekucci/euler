af7fd338da4ed03d9fe6672665d689e453b3fedd
Peter Mrekaj <mrekucci@gmail.com>
2015-01-27 17:41:42 +0100

    Improve solution of problem 1
    Remove throwing of unnecessary error, which results to improve readability of
    code.

--- problem001/problem001.go
+++ problem001/problem001.go
@@ -4,33 +4,13 @@
 
 package problem001
 
-import "fmt"
-
 // Solution for finding the sum of all the multiples of 3 or 5 below 1000.
 func Solution() int {
-	var s3, s5, s15 int
-	var err error
-
-	if s3, err = sn(3, 999); err != nil {
-		panic(err)
-	}
-	if s5, err = sn(5, 999); err != nil {
-		panic(err)
-	}
-	if s15, err = sn(15, 999); err != nil {
-		panic(err)
-	}
-	return s3 + s5 - s15
+	return sn(3, 999) + sn(5, 999) - sn(15, 999)
 }
 
-// sn returns the sum of arithmetic sequence
-// up to the number an. The sequence has difference d.
-// An error is returned when d >= an.
-func sn(d, an int) (int, error) {
-	if d >= an {
-		return 0, fmt.Errorf("illegal argument %d >= %d; want %d < %d", d, an, d, an)
-	}
-
+// sn returns the sum of arithmetic sequence from 0 up to the an with difference d.
+func sn(d, an int) int {
 	n := an / d
-	return int(float64(n) / 2 * float64((2*d)+(n-1)*d)), nil
+	return int(float64(n) / 2 * float64((2*d)+(n-1)*d))
 }
--- problem001/problem001_test.go
+++ problem001/problem001_test.go
@@ -14,13 +14,25 @@ func TestSolution(t *testing.T) {
 	util.AssertEquals(t, Solution(), 233168)
 }
 
+var snTests = []struct {
+	d, an int
+	out   int
+}{
+	{1, 5, 15},
+	{2, 10, 30},
+	{3, 15, 45},
+	{4, 1000, 125500},
+	{5, 1000, 100500},
+	{10, 1000, 50500},
+}
+
 func TestSn(t *testing.T) {
-	d, an := 1, 5
-	r, err := sn(d, an)
-	if err != nil {
-		t.Fatalf("sn(%d, %d) returns error: %s", d, an, err)
+	for _, tt := range snTests {
+		got := sn(tt.d, tt.an)
+		if got != tt.out {
+			t.Errorf("sn(%d, %d) = %d; want %d", tt.d, tt.an, got, tt.out)
+		}
 	}
-	util.AssertEquals(t, r, 15)
 }
 
 func BenchmarkSolution(b *testing.B) {
