d2ecee1978c41e20c5de27ed789d10d0a655bcd4
Peter Mrekaj <mrekucci@gmail.com>
2015-01-31 08:38:54 +0100

    Solution of problem 13
    https://projecteuler.net/problem=13

---
+++ problem13_test.go
@@ -0,0 +1,21 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "testing"
+
+func TestProblem13(t *testing.T) {
+	want := 5537376230
+	got, err := Problem13()
+	if got != want || err != nil {
+		t.Errorf("Problem13() = %d, %v; want %d, <nil>", got, err, want)
+	}
+}
+
+func BenchmarkProblem13(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem13()
+	}
+}
