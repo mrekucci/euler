3f11b19208027cf8464afa21a2ec5d26ba128b26
Peter Mrekaj <mrekucci@gmail.com>
2015-02-03 08:39:21 +0100

    Solution of problem 16
    https://projecteuler.net/problem=16

---
+++ problem16.go
@@ -0,0 +1,17 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "math/big"
+
+// Problem16 is solution for finding the sum of the digits of the number 2^1000.
+func Problem16() int {
+	num := big.NewInt(1).Exp(big.NewInt(2), big.NewInt(1000), nil).String()
+	sum := 0
+	for _, d := range num {
+		sum += int(d - '0')
+	}
+	return sum
+}
---
+++ problem16_test.go
@@ -0,0 +1,21 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "testing"
+
+func TestProblem16(t *testing.T) {
+	want := 1366
+	got := Problem16()
+	if got != want {
+		t.Errorf("Problem16() = %d; want %d", got, want)
+	}
+}
+
+func BenchmarkProblem16(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem16()
+	}
+}
