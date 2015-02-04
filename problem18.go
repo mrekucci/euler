cf13e0165b8c3b4b9caad7a0c4f878fa21f99874
Peter Mrekaj <mrekucci@gmail.com>
2015-02-04 15:46:01 +0100

    Solution of problem 18
    https://projecteuler.net/problem=18

---
+++ problem18.go
@@ -0,0 +1,37 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "math"
+
+// Problem18 is solution for finding the maximum
+// total from top to bottom of the triangle.
+func Problem18() int {
+	triangle := [][]float64{
+		{75},
+		{95, 64},
+		{17, 47, 82},
+		{18, 35, 87, 10},
+		{20, 04, 82, 47, 65},
+		{19, 01, 23, 75, 03, 34},
+		{88, 02, 77, 73, 07, 63, 67},
+		{99, 65, 04, 28, 06, 16, 70, 92},
+		{41, 41, 26, 56, 83, 40, 80, 70, 33},
+		{41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+		{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+		{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+		{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+		{63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+		{04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23},
+	}
+
+	// Next solution is base on dynamic programming.
+	for x := len(triangle) - 1; x > 0; x-- {
+		for y := 0; y < len(triangle[x])-1; y++ {
+			triangle[x-1][y] += math.Max(triangle[x][y], triangle[x][y+1])
+		}
+	}
+	return int(triangle[0][0])
+}
---
+++ problem18_test.go
@@ -0,0 +1,21 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "testing"
+
+func TestProblem18(t *testing.T) {
+	want := 1074
+	got := Problem18()
+	if got != want {
+		t.Errorf("Problem18() = %d; want %d", got, want)
+	}
+}
+
+func BenchmarkProblem18(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem18()
+	}
+}
