f8a48201a35c3333c14ad07e756dec0f79869ecb
Peter Mrekaj <mrekucci@gmail.com>
2015-02-05 09:23:44 +0100

    Solution of problem 19
    https://projecteuler.net/problem=19

---
+++ problem19.go
@@ -0,0 +1,68 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+// Problem19 is solution for finding how many Sundays fell on the first
+// of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000).
+func Problem19() int {
+	// First solution base on time package:
+	//
+	//	cnt := 0
+	//	for y := 1901; y <= 2000; y++ {
+	//		for m := 1; m <= 12; m++ {
+	//			if time.Date(y, time.Month(m), 1, 0, 0, 0, 0, time.UTC).Weekday() == time.Sunday {
+	//				cnt++
+	//			}
+	//		}
+	//	}
+	//	return cnt
+	//
+
+	// Second solution without using time package, longer but faster.
+	months := []int{
+		31, // Jan
+		28, // Feb
+		31, // Mar
+		30, // Apr
+		31, // May
+		30, // Jun
+		31, // Jul
+		31, // Aug
+		30, // Sep
+		31, // Oct
+		30, // Nov
+		31, // Dec
+	}
+
+	cnt := 0
+	day := 2 // Represents the day 0-6 => Sun to Sat. We start with '2' because it was Tuesday on 1/1/1901.
+	for y := 1901; y <= 2000; y++ {
+		ly := isLeapYear(y)
+		for m, d := range months {
+			if m == 1 && ly { // Check if Feb. should have 29 days.
+				d++
+			}
+
+			day += (d % 28) // Add remaining days of month, this shift us to the end of the month.
+			// The actual day can be computed as day%7, 'cause %7 generates day from 0 to 6
+			// day+1 shift us at the first day of the next month.
+			if (day+1)%7 == 0 {
+				cnt++
+				// We can reset the day (day %= 7) to 0-6 after statement cnt++
+				// But this isn't necessary, because we care only about the
+				// remainder. It will be always the same with or without the reset.
+			}
+		}
+	}
+	return cnt
+}
+
+// isLeapYear returns true if y is a leap year.
+func isLeapYear(y int) bool {
+	if y%400 == 0 || y%4 == 0 && !(y%100 == 0) {
+		return true
+	}
+	return false
+}
---
+++ problem19_test.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package euler
+
+import "testing"
+
+func TestProblem19(t *testing.T) {
+	want := 171
+	got := Problem19()
+	if got != want {
+		t.Errorf("Problem19() = %d; want %d", got, want)
+	}
+}
+
+var isLeapYearTests = []struct {
+	in  int
+	out bool
+}{
+	{1904, true},
+	{1910, false},
+	{1920, true},
+	{1930, false},
+	{1932, true},
+	{1950, false},
+	{1955, false},
+	{1960, true},
+	{1970, false},
+	{1980, true},
+	{1990, false},
+	{2000, true},
+}
+
+func TestIsLapYear(t *testing.T) {
+	for _, tt := range isLeapYearTests {
+		got := isLeapYear(tt.in)
+		if got != tt.out {
+			t.Errorf("isLeapYear(%d) = %t; want %t", tt.in, got, tt.out)
+		}
+	}
+}
+
+func BenchmarkProblem19(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem19()
+	}
+}
