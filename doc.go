be21f93196a1134b92cb0d7fedb1fffd8a5ed78c
Peter Mrekaj <mrekucci@gmail.com>
2015-02-11 15:56:51 +0100

    Add package documentation
    Package documentation explains his intention.

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2015 Peter Mrekaj. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package euler contains solutions for project Euler problems in golang.
+// See: https://projecteuler.net
+package euler
