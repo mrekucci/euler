0e02ae0190604416e2ed42d390e5d88fa80bdda5
Peter Mrekaj <mrekucci@gmail.com>
2015-08-15 17:24:36 -0700

    Fix potential bug in the Problem1

--- problem1.go
+++ problem1.go
@@ -4,6 +4,8 @@
 
 package euler
 
+import "math"
+
 // Problem1 is solution for finding the sum of all the multiples of 3 or 5 below 1000.
 func Problem1() int {
 	return sn(3, 999) + sn(5, 999) - sn(15, 999)
@@ -11,8 +13,6 @@ func Problem1() int {
 
 // sn returns the sum of arithmetic sequence from 0 up to the an with difference d.
 func sn(d, an float64) int {
-	// 'n' computation and truncation (faster then math.Trunc).
-	// We truncate an / d 'cause we need an integer to perform correct computation.
-	n := float64(int(an / d))
+	n := math.Trunc(an / d) // We truncate an / d 'cause we need an integer to perform correct computation.
 	return int(n / 2 * ((2 * d) + (n-1)*d))
 }
